355238a2ba9c39a46b55d0b52fc61905516df26b
arun chavan <arunchavan89@gmial.com>
2019-07-18 12:20:38 +0200

    first unstable solution

--- src/route_model.h
+++ src/route_model.h
@@ -6,27 +6,47 @@
 #include "model.h"
 #include <iostream>
 
-class RouteModel : public Model {
+class RouteModel : public Model
+{
 
-  public:
-    class Node : public Model::Node {
-      public:
+public:
+    class Node : public Model::Node
+    {
+    public:
         // Add public Node variables and methods here.
-        
-        Node(){}
-        Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
-      private:
+
+        Node() {}
+        Node(int idx, RouteModel* search_model, Model::Node node) : Model::Node(node), parent_model(search_model) { index = idx; }
+        Node* parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node*> neighbors;
+
+        float distance(Node obj_node) const
+        {
+            return std::sqrt(std::pow((x - obj_node.x), 2) + std::pow((y - obj_node.y), 2));
+        }
+
+        void FindNeighbors();
+
+    private:
         // Add private Node variables and methods here.
         int index;
-        RouteModel * parent_model = nullptr;
+        RouteModel* parent_model = nullptr;
+        Node* FindNeighbor(std::vector<int> node_indices);
     };
-    
+
     // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
+    RouteModel(const std::vector<std::byte>& xml);
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    auto& SNodes() { return m_Nodes; }
+    Node& FindClosestNode(float x, float y);
+    auto& GetNodeToRoadMap() { return node_to_road; }
 
-  private:
+private:
     // Add private RouteModel variables and methods here.
-
+    std::vector<Node> m_Nodes;
+    std::unordered_map<int, std::vector<const Model::Road*>> node_to_road;
+    void CreateNodeToRoadHashmap();
 };
